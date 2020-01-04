43e43426bc87b6a267e31e1bddf8a52c3771a522
Nagisa Kojima <nk.bg.1208@gmail.com>
2020-01-05 08:40:37 +0900

    Todo component added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
+import todoApp from './reducers'
+const store = createStore(todoApp)
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
